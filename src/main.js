1220c7dc97b8fcc808d9f09b605696bebbbbd106
wahjoeh <wahyu.22099@mhs.unesa.ac.id>
2026-06-19 07:20:55 +0700

    Complete TOEFL ITP simulation project

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { inject } from '@vercel/analytics'
 
-createApp(App)
-  .use(router)
-  .mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
+
+inject()
